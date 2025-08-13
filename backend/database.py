c8324aaa20cf09a4a4d55b1dec86462d73202612
unknown <elkhadiriniama@gmail.com>
2025-08-13 11:57:41 +0200

    modificayion database.py

--- backend/database.py
+++ backend/database.py
@@ -17,9 +17,10 @@ SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 # Base pour les modèles
 Base = declarative_base()
-# Test de connexion
-try:
-    with engine.connect() as connection:
-        print("Connexion réussie à la base de données")
-except Exception as e:
-    print("Erreur de connexion :", e)
+# Création des tables dans la base
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    print("Tables créées avec succès")
+
+from models import user
+
